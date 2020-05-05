b85a1319f33c580ea245aed2e357d57bdc2cace3
PavliukKonstantin <PavliukKonstantin@users.noreply.github.com>
2020-05-05 15:15:59 +0300

    Codewars add 'pig_it'

---
+++ Codewars/Tests/test_pig_it.py
@@ -0,0 +1,9 @@
+from Codewars.pig_it import pig_it
+
+
+def test_pig_it():
+    assert pig_it('This is my string') == 'hisTay siay ymay tringsay'
+    assert pig_it('Pig latin is cool') == 'igPay atinlay siay oolcay'
+    assert pig_it(
+        "Quis custodiet ipsos custodes ?"
+        ) == 'uisQay ustodietcay psosiay ustodescay ?'
---
+++ Codewars/pig_it.py
@@ -0,0 +1,18 @@
+# Move the first letter of each word to the end of it,
+# then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+# Examples
+# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+# pig_it('Hello world !')     # elloHay orldway !
+
+# def pig_it(text):
+#     lst = text.split()
+#     return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
+
+def pig_it(text):
+    words = text.split()
+    signs = {"!", "?"}
+    return " ".join([("{0}{1}ay".format(i[1:], i[0]), i)[i in signs] for i in words])
+
+
+print(pig_it("Quis custodiet ipsos custodes ?"))
