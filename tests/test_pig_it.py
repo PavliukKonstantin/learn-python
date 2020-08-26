3319f60dea252927deadad8a02d24617dbdc6f37
PavliukKonstantin <Pavliuk.Konstantin@gmail.com>
2020-08-26 13:55:11 +0300

    Delete tests

--- tests/test_pig_it.py
+++
@@ -1,7 +0,0 @@
-from learn_python.codewars.pig_it import pig_it
-
-
-def test_pig_it():
-    assert pig_it('This is my string') == 'hisTay siay ymay tringsay'
-    assert pig_it('Pig latin is cool') == 'igPay atinlay siay oolcay'
-    assert pig_it("Quis custodiet ipsos custodes ?") == 'uisQay ustodietcay psosiay ustodescay ?'
